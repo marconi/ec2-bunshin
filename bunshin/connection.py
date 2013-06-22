8b001233d8b07c072404d9b4e9d7d8e45c26beac
Marconi Moreto <caketoad@gmail.com>
2013-06-22 17:37:16 +0800

    removed debugging lines and added docstring

--- bunshin/connection.py
+++ bunshin/connection.py
@@ -4,8 +4,7 @@ from .config import AWS_ACCESS_KEY, AWS_SECRET_KEY
 
 
 def connect_to_region(region_name):
-    print AWS_SECRET_KEY
-    print AWS_ACCESS_KEY
+    """ Connect to an AWS region. """
     return ec2.connect_to_region(region_name=region_name,
                                  aws_access_key_id=AWS_ACCESS_KEY,
                                  aws_secret_access_key=AWS_SECRET_KEY)
